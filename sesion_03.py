5d9dc592dc3c9abf5900b51a4458a81ab18c5360
levigss <levxgss@gmail.com>
2026-02-12 17:29:25 -0800

    pythonbasico

--- sesion_03.py
+++ sesion_03.py
@@ -19,3 +19,52 @@ for i in range(2, 9):
 for i in mi_lista_2:
     if i != "Lunes":
             print(f"Feliz{i}!")
+
+
+# While loop
+i = 0
+
+while i < 5:
+     i += 1
+     if i == 3:
+          continue
+     print(i)
+     if i == 4:
+          break
+
+else:
+     print("i es ahora mayor o igual a 5")
+
+#Practica 2
+# Dada la lista mi_lista_2 = ["lunes", "martes", "miercoles", "jueves",  "viernes"]
+# Imprime cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+# Pista: Usalos dos tipos loops while y for en el mismo programa para lograrlo
+# Resultado:
+# martes
+# miercoles
+# jueves 
+# viernes 
+# martes
+# miercoles 
+# jueves
+# viernes
+# martes
+# miercoles
+# jueves
+# viernes
+
+mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+
+# Inicializamos un contador para el bucle while
+repeticiones = 0
+
+# El bucle while se encargará de repetir todo el proceso 3 veces
+while repeticiones < 3:
+    # El bucle for recorre cada elemento de la lista
+    for dia in mi_lista_2:
+        # Usamos un condicional para saltarnos el "lunes"
+        if dia != "lunes":
+            print(dia)
+    
+    # Incrementamos el contador para no crear un bucle infinito
+    repeticiones += 1
